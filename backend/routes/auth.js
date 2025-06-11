e663c14340427cfbb02eef50c71494162db45f6f
Arnav Oruganty <Arnav.Oruganty@iiitb.ac.in>
2025-06-11 19:15:24 +0530

    changes to auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,6 +10,11 @@ const {
 //Get all signed up users
 router.get('/signup', getUsers);
 
+// Get all logged in users
+router.get('/login', (req, res) => {
+    res.status(200).json({ message: 'Login route' });
+});
+
 // Signup Route
 router.post('/signup', signUp);
 
