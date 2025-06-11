25524878257b44b92e9afce05a6ba44ec14f71cd
Arnav Oruganty <Arnav.Oruganty@iiitb.ac.in>
2025-06-11 19:08:24 +0530

    error fixing in login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,12 +2,12 @@ import { Box, Typography, TextField, Button, Paper } from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const navigate = useNavigate();
-
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
